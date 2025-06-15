51d96f6caec57cb69e3acd2d62ba5539a0d8822c
Hilary <hilarykaior@gmail.com>
2025-06-15 11:40:53 +0100

    PhaseGateOne

--- java/Student grade/StudentGrade.java
+++ java/Student grade/StudentGrade.java
@@ -14,6 +14,14 @@ public static void main(String [] args){
 		for(int stCol =0; stCol < dosier[stRow].length; stCol++){
 			System.out.print("Subject" + (stCol + 1) + ": ");
 			dosier[stRow][stCol] = scan.nextInt();
+			while (true){
+				if(dosier[stRow][stCol] < 0 || dosier[stRow][stCol] > 100){
+					System.out.print("Scores must be between 1 and 100 only");
+					}
+				else{
+					break;
+					}
+				}
 			}	
 		}
 	int[] total = new int[numberOfStudent];
@@ -44,6 +52,33 @@ public static void main(String [] args){
 			System.out.print(dosier[row][col] + "\t");
 			}
 		System.out.printf("%d\t%.2f\t%d\n", total[row], average[row], position[row]);
+		}
+		
+
+		int sumOfSubject = 0;
+		int averageOfSubject = 0;
+		int pass = 0;
+		int fail = 0;
+		int count = 0;
+
+	for(int row = 0; row < dosier.length; row++){
+		System.out.println("SUBJECT SUMMARY\nSUBJECT" + (row + 1) + ":");
+		for(int col = 0; col < dosier.length; col++){
+		if(dosier[col][count] > dosier[col][count]){
+		System.out.print("Highest scoring student is: Student " + (row + 1) + "Scoring " + dosier[row][count]);
+		}else{
+			System.out.print("Lowest scoring student is: Student " + (row + 1) + "Scoring " + dosier[row][count]);
+			}
+		if(dosier[col][count] > 40){
+			pass += 1;
+			}else{ fail += 1;}
+		sumOfSubject += dosier[col][count]; 
+		}
+			averageOfSubject = sumOfSubject / numberOfStudent;
+			System.out.print("Total is: " + sumOfSubject);
+			System.out.print("Average is: " + averageOfSubject);
+			System.out.print("Number of passes: " + pass);
+			System.out.print("Number of failures: " + fail);
 		}
 }
 }  
